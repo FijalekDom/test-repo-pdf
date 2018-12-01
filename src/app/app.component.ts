8ee41fafbefd0910b74c60f818900d235b3873f4
Dominik <dominikfijalek@gmail.com>
2018-12-01 19:32:05 +0100

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClickService} from './click.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ClickService]
 })
-export class AppComponent {
-  title = 'test-project';
+export class AppComponent implements OnInit {
+
+
+
+    constructor(  ) {
+    }
+
+    ngOnInit(): void {
+
+}
+
 }
---
+++ src/app/klikacz1/klikacz1.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ClickService} from '../click.service';
+
+@Component({
+  selector: 'app-klikacz1',
+  templateUrl: './klikacz1.component.html',
+  styleUrls: ['./klikacz1.component.css'],
+})
+export class Klikacz1Component implements OnInit {
+
+  click = 0;
+
+  constructor( private clickService: ClickService ) { }
+
+  ngOnInit() {
+  }
+
+  add(): void {
+    this.click += 1;
+    this.clickService.addClicks();
+  }
+
+}
